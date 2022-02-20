0f29cc044ce7f69aaf3be79501ae42a32e88f015
MSI\Jim <jim60308@gmail.com>
2022-02-20 20:58:08 +0800

    功能完成

--- src/widgets/SpatialQuery/index.tsx
+++ src/widgets/SpatialQuery/index.tsx
@@ -7,14 +7,27 @@ import ClearIcon from '@mui/icons-material/Clear'
 import { arcGisContext } from '../../lib/MapProvider'
 import * as watchUtils from '@arcgis/core/core/watchUtils'
 import MapView from '@arcgis/core/views/MapView'
-import { projectExtent } from '../../utils/modules/Extent'
+import { projectExtent, geometry2Extent } from '../../utils/modules/Extent'
+import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer'
+import Sketch from '@arcgis/core/widgets/Sketch'
+
+const mockSketch = {
+  clickDrawingButton: (sketchId: string) => {
+    const rectangleBtn = document.getElementById(sketchId)?.getElementsByTagName('calcite-action')[1] as HTMLButtonElement
+    rectangleBtn.click()
+  },
+  clickPanButton: (sketchId: string) => {
+    const cursorBtn = document.getElementById(sketchId)?.getElementsByTagName('calcite-action')[0] as HTMLButtonElement
+    cursorBtn.click()
+  }
+}
 
 type mode = 'current' | 'manual'
 type btnMode = 'default' | 'info'
 
 const SpatialQuery = () => {
   const arcgis = useContext(arcGisContext)
-  const [currentMode, setcurrentMode] = useState<mode>('current')
+  const [, setcurrentMode] = useState<mode>('current')
   const [xmin, setxmin] = useState<number | undefined>(undefined)
   const [ymin, setymin] = useState<number | undefined>(undefined)
   const [xmax, setxmax] = useState<number | undefined>(undefined)
@@ -22,36 +35,88 @@ const SpatialQuery = () => {
   const [precision] = useState<number>(1000)
   const [watchHandle, setwatchHandle] = useState<any | undefined>(undefined)
   const [btnStateArray, setbtnStateArray] = useState<Array<btnMode>>(['info', 'default'])
+  const [sketchId] = useState<string>('sketchTool')
+  const [currentSketch, setcurrentSketch] = useState<Sketch | undefined>(undefined)
+  // const [, setsketchLayer] = useState<GraphicsLayer | undefined>(undefined)
 
   const startWatchingExtent = () => {
-    setwatchHandle(
-      watchUtils.whenTrue(arcgis.mapView as MapView, 'stationary', () => {
-        console.log(currentMode)
-        if (arcgis.mapView?.extent) {
-          const _extent = projectExtent(arcgis.mapView)
-          setxmin(_extent[0])
-          setymin(_extent[1])
-          setxmax(_extent[2])
-          setymax(_extent[3])
-        }
-      })
-    )
+    if (watchHandle === undefined) {
+      if (currentSketch) {
+        mockSketch.clickPanButton(sketchId)
+      }
+      setwatchHandle(
+        watchUtils.whenTrue(arcgis.mapView as MapView, 'stationary', () => {
+          if (arcgis.mapView?.extent) {
+            const _extent = projectExtent(arcgis.mapView)
+            setxmin(_extent[0])
+            setymin(_extent[1])
+            setxmax(_extent[2])
+            setymax(_extent[3])
+          }
+        })
+      )
+    }
+  }
+
+  const initSketchWidget = () => {
+    const layer = new GraphicsLayer({ listMode: 'hide' })
+    arcgis.map?.add(layer)
+    const sketch = new Sketch({
+      view: arcgis.mapView,
+      layer: layer,
+      container: document.getElementById(sketchId) as HTMLElement,
+      visibleElements: {
+        createTools: {
+          point: false,
+          circle: false,
+          polygon: false,
+          polyline: false
+        },
+        selectionTools: {
+          'rectangle-selection': false,
+          'lasso-selection': false
+        },
+        undoRedoMenu: false,
+        settingsMenu: false
+      }
+    })
+    sketch.on('create', (event) => {
+      if (event.state === 'complete') {
+        layer.remove(event.graphic)
+        const _extent = geometry2Extent(event.graphic.geometry)
+        setxmin(_extent[0])
+        setymin(_extent[1])
+        setxmax(_extent[2])
+        setymax(_extent[3])
+      }
+    })
+    setcurrentSketch(sketch)
+    // (window as any).sketch = sketch
+  }
+
+  const startManualMode = () => {
+    if (currentSketch?.activeTool === null) {
+      mockSketch.clickDrawingButton(sketchId)
+    }
   }
 
   useEffect(() => {
     startWatchingExtent()
+    initSketchWidget()
   }, [])
 
   const handleSetCurrentExtent = () => {
-    setbtnStateArray(['info', 'default'])
     setcurrentMode('current')
+    setbtnStateArray(['info', 'default'])
     startWatchingExtent()
   }
 
   const handleManualSetExtent = () => {
+    setcurrentMode('manual')
     setbtnStateArray(['default', 'info'])
     watchHandle?.remove()
-    setcurrentMode('manual')
+    setwatchHandle(undefined)
+    startManualMode()
   }
 
   const handleClearExtent = () => {
@@ -63,6 +128,8 @@ const SpatialQuery = () => {
 
   return (
     <div className='SpatialQuery'>
+      <div id={sketchId} className='sketch-tool'></div>
+
       <div className='manipulate-group'>
         <Tooltip title="目前地圖範圍">
           <IconButton onClick={handleSetCurrentExtent} color={btnStateArray[0]}>
